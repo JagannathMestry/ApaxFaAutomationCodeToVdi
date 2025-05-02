9874f1e8e5094d2e2d2a44ac37b5851aaaa16ca3
Jagannath_Mestry <163306083+JagannathMestry@users.noreply.github.com>
2025-05-02 13:17:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,60 +1,8 @@
-const https = require('https');
-
-module.exports = async function (context, req) {
-    context.log('Function started: testing GET request with built-in https');
-
-    const url = '';
-    const token = ' ';  // Replace with your actual token
-
-    const fetchData = (url, token) => {
-        return new Promise((resolve, reject) => {
-         
-            const options = {
-                headers: {
-                    'Content-Type':'application/json',
-                    'Authorization': `Bearer ${token}`
-                }
-            };
-
-            https.get(url, options, (res) => {
-                let data = '';
-
-                res.on('data', (chunk) => {
-                    data += chunk;
-                });
-
-                res.on('end', () => {
-                    try {
-                        resolve(JSON.parse(data));
-                    } catch (err) {
-                        reject(err);
-                    }
-                });
-            }).on('error', (err) => {
-                reject(err);
-            });
-        });
-    };
-
-    try {
-        const data = await fetchData(url, token);
-
-        context.res = {
-            status: 200,
-            body: {
-                message: "GET request successful using built-in https.",
-                data: data
+ {
+        "PayeeCode": "19667",
+        "PayeeDescription": {
+            "Fields": {
+                "IsActive": "false"
             }
-        };
-    } catch (error) {
-        context.log('Request failed:', error.message);
-
-        context.res = {
-            status: 500,
-            body: {
-                message: "GET request failed.",
-                error: error.message
-            }
-        };
+        }
     }
-};
